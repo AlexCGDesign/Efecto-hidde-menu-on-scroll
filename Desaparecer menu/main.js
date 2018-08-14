b6d64def2e09a46fd372a034f0e8abc3e998e526
Jordan Alexander Cruz - AlexCG Design <37056514+AlexCGDesign@users.noreply.github.com>
2018-08-14 10:14:44 -0500

    Add files via upload

---
+++ Desaparecer menu/main.js
@@ -0,0 +1,11 @@
+let ubicacionPrincipal  = window.pageYOffset;
+window.onscroll = function() {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if(ubicacionPrincipal >= Desplazamiento_Actual){
+        document.getElementById('navbar').style.top = '0';
+    }
+    else{
+        document.getElementById('navbar').style.top = '-100px';
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+}
